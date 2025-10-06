b96b3fd2b59cea68c6e0b9fcbfdbf12f64b25101
Alberto Cozzani <acozzani@login01.leonardo.local>
2025-10-06 15:18:40 +0200

    Added dataset files

---
+++ dataset/augment.py
@@ -0,0 +1,6 @@
+from dataset import add_files
+
+output_dir = 'danish_bbr_google_noherlev/gentofte_trainval/train/negative/'
+input_dir = 'danish_bbr_google_noherlev/herlev_test/test/negative/'
+add_files(1048, input_dir, output_dir)
+
